1a238202b07d85074892355fa5d7dd453499a087
HananMoAlnakhal <nana2004mo@gmail.com>
2025-06-21 12:31:04 +0300

    ngramPredictNext

--- Analyzer.py
+++ Analyzer.py
@@ -9,11 +9,18 @@ import string
 class SmartTextAnalyzer():
     stopWords=['dr', 'dra', 'mr', 'ms', 'a', "a's", 'able', 'about', 'above', 'according', 'accordingly', 'across', 'actually', 'after', 'afterwards', 'again', 'against', "ain't", 'all', 'allow', 'allows', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'am', 'among', 'amongst', 'an', 'and', 'another', 'any', 'anybody', 'anyhow', 'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'appear', 'appreciate', 'appropriate', 'are', "aren't", 'around', 'as', 'aside', 'ask', 'asking', 'associated', 'at', 'available', 'away', 'awfully', 'b', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'believe', 'below', 'beside', 'besides', 'best', 'better', 'between', 'beyond', 'both', 'brief', 'but', 'by', 'c', "c'mon", "c's", 'came', 'can', "can't", 'cannot', 'cant', 'cause', 'causes', 'certain', 'certainly', 'changes', 'clearly', 'co', 'com', 'come', 'comes', 'concerning', 'consequently', 'consider', 'considering', 'contain', 'containing', 'contains', 'corresponding', 'could', "couldn't", 'course', 'currently', 'd', 'definitely', 'described', 'despite', 'did', "didn't", 'different', 'do', 'does', "doesn't", 'doing', "don't", 'done', 'down', 'downwards', 'during', 'e', 'each', 'edu', 'eg', 'eight', 'either', 'else', 'elsewhere', 'enough', 'entirely', 'especially', 'et', 'etc', 'even', 'ever', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'ex', 'exactly', 'example', 'except', 'f', 'far', 'few', 'fifth', 'first', 'five', 'followed', 'following', 'follows', 'for', 'former', 'formerly', 'forth', 'four', 'from', 'further', 'furthermore', 'g', 'get', 'gets', 'getting', 'given', 'gives', 'go', 'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'h', 'had', "hadn't", 'happens', 'hardly', 'has', "hasn't", 'have', "haven't", 'having', 'he', "he's", 'hello', 'help', 'hence', 'her', 'here', "here's", 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'hi', 'him', 'himself', 'his', 'hither', 'hopefully', 'how', 'howbeit', 'however', 'i', "i'd", "i'll", "i'm", "i've", 'ie', 'if', 'ignored', 'immediate', 'in', 'inasmuch', 'inc', 'indeed', 'indicate', 'indicated', 'indicates', 'inner', 'insofar', 'instead', 'into', 'inward', 'is', "isn't", 'it', "it'd", "it'll", "it's", 'its', 'itself', 'j', 'just', 'k', 'keep', 'keeps', 'kept', 'know', 'knows', 'known', 'l', 'last', 'lately', 'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', "let's", 'like', 'liked', 'likely', 'little', 'look', 'looking', 'looks', 'ltd', 'm', 'mainly', 'many', 'may', 'maybe', 'me', 'mean', 'meanwhile', 'merely', 'might', 'more', 'moreover', 'most', 'mostly', 'much', 'must', 'my', 'myself', 'n', 'name', 'namely', 'nd', 'near', 'nearly', 'necessary', 'need', 'needs', 'neither', 'never', 'nevertheless', 'new', 'next', 'nine', 'no', 'nobody', 'non', 'none', 'noone', 'nor', 'normally', 'not', 'nothing', 'novel', 'now', 'nowhere', 'o', 'obviously', 'of', 'off', 'often', 'oh', 'ok', 'okay', 'old', 'on', 'once', 'one', 'ones', 'only', 'onto', 'or', 'other', 'others', 'otherwise', 'ought', 'our', 'ours', 'ourselves', 'out', 'outside', 'over', 'overall', 'own', 'p', 'particular', 'particularly', 'per', 'perhaps', 'placed', 'please', 'plus', 'possible', 'presumably', 'probably', 'provides', 'q', 'que', 'quite', 'qv', 'r', 'rather', 'rd', 're', 'really', 'reasonably', 'regarding', 'regardless', 'regards', 'relatively', 'respectively', 'right', 's', 'said', 'same', 'saw', 'say', 'saying', 'says', 'second', 'secondly', 'see', 'seeing', 'seem', 'seemed', 'seeming', 'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven', 'several', 'shall', 'she', 'should', "shouldn't", 'since', 'six', 'so', 'some', 'somebody', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry', 'specified', 'specify', 'specifying', 'still', 'sub', 'such', 'sup', 'sure', 't', "t's", 'take', 'taken', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', "that's", 'thats', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'thence', 'there', "there's", 'thereafter', 'thereby', 'therefore', 'therein', 'theres', 'thereupon', 'these', 'they', "they'd", "they'll", "they're", "they've", 'think', 'third', 'this', 'thorough', 'thoroughly', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'took', 'toward', 'towards', 'tried', 'tries', 'truly', 'try', 'trying', 'twice', 'two', 'u', 'un', 'under', 'unfortunately', 'unless', 'unlikely', 'until', 'unto', 'up', 'upon', 'us', 'use', 'used', 'useful', 'uses', 'using', 'usually', 'uucp', 'v', 'value', 'various', 'very', 'via', 'viz', 'vs', 'w', 'want', 'wants', 'was', "wasn't", 'way', 'we', "we'd", "we'll", "we're", "we've", 'welcome', 'well', 'went', 'were', "weren't", 'what', "what's", 'whatever', 'when', 'whence', 'whenever', 'where', "where's", 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 'whither', 'who', "who's", 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'willing', 'wish', 'with', 'within', 'without', "won't", 'wonder', 'would', 'would', "wouldn't", 'x', 'y', 'yes', 'yet', 'you', "you'd", "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'z', 'zero']
     punctuation="!@#$%^&*()/|\\,._--=+><?'\"}{[:;؛ّ~`]â€"
+    contractions = {"ain't": "is not","aren't": "are not","can't": "cannot","couldn't": "could not","didn't": "did not","doesn't": "does not","don't": "do not","hadn't": "had not","hasn't": "has not","haven't": "have not","isn't": "is not","mightn't": "might not","mustn't": "must not","needn't": "need not","shan't": "shall not","shouldn't": "should not","wasn't": "was not","weren't": "were not","won't": "will not","wouldn't": "would not"}
+    other_contractions = {"i'm": "i am","you're": "you are","he's": "he is","she's": "she is","it's": "it is","we're": "we are","they're": "they are","i've": "i have","you've": "you have","we've": "we have","they've": "they have","who's": "who is","what's": "what is","where's": "where is","when's": "when is","why's": "why is","how's": "how is","i'd": "i would","you'd": "you would","he'd": "he would","she'd": "she would","we'd": "we would","they'd": "they would","i'll": "i will","you'll": "you will","he'll": "he will","she'll": "she will","we'll": "we will","they'll": "they will","there's": "there is","here's": "here is","let's": "let us","that's": "that is","who'd": "who would","who'll": "who will","who've": "who have","y'all": "you all"}
+    negates={"never","no","nothing","nowhere","noone","none","not"}
 
-    def __init__(self,TextInput=""):
+    all_contractions = {**contractions, **other_contractions}
+
+    def __init__(self,TextInput="",nGram=3):
         self.text=TextInput
         self.sentences=[]
         self.words=[]
+        self.ngram_size = nGram if nGram>=2 else 2
+        self.ngram = {}
         if self.text!="":
             self.sentences=self.getSentences()
             self.words=self.getWords()
@@ -51,28 +58,51 @@ class SmartTextAnalyzer():
         self.CharacterCount=0
         self.CharacterOccurrence={}
         words=[]
-        for sentence in self.sentences:
-            wordsInSentence=[]
-            for word in sentence.split():
-                processed=""
+        for ind,sentence in enumerate(self.sentences):
+            expanded_sentence = self.expand_contractions(sentence, __class__.all_contractions)
+            wordsInSentence = []
+            for word in expanded_sentence.split():
+                processed = ""
                 for letter in word:
-                    if letter!=" ":
-                        self.CharacterCount+=1
-                        __class__.__increment(self.CharacterOccurrence,letter)
+                    if letter != " ":
+                        self.CharacterCount += 1
+                        __class__.__increment(self.CharacterOccurrence, letter)
                     if letter in __class__.punctuation:
                         continue
-                    processed+=letter
-                if processed.isalpha() :
+                    processed += letter
+                if processed.isalpha():
                     wordsInSentence.append(processed)
-                    self.totalWords+=1
-                    __class__.__increment(self.wordsCounts,processed)
-                    if not(processed in __class__.stopWords):
-                        __class__.__increment(self.uniqueWords,processed)
+                    self.totalWords += 1
+                    __class__.__increment(self.wordsCounts, processed)
+                    if processed not in __class__.stopWords:
+                        __class__.__increment(self.uniqueWords, processed)
+
+            self.add_ngram(wordsInSentence)
             words.append(wordsInSentence)
 
-        self.uniqueCount=sum(self.uniqueWords.values())
+        self.uniqueCount = sum(self.uniqueWords.values())
         return words
 
+    def expand_contractions(self,sentence, contractions_dict):
+        words = sentence.split()
+        expanded_words = []
+        for word in words:
+            lower_word = word.lower()
+            if lower_word in contractions_dict:
+                expanded_words.extend(contractions_dict[lower_word].split())
+            else:
+                found = False
+                for suffix, replacement in contractions_dict.items():
+                    if word.lower().endswith(suffix):
+                        base = word[:-len(suffix)]
+                        if base:
+                            expanded_words.append(base)
+                        expanded_words.extend(replacement.split())
+                        found = True
+                        break
+                if not found:
+                    expanded_words.append(word)
+        return ' '.join(expanded_words)
 ########################################################################################
 #      Search the Text
 #---------------------------------------------------------------------------------------
@@ -88,17 +118,17 @@ class SmartTextAnalyzer():
             clean=clean+letter.lower()
         Target=clean
         for sentence_num,sentence in enumerate(self.sentences):
-            # s=" ".join(sentence)
             s=sentence
-            pos=s.find(Target)
+            pos=s.lower().find(Target)
             wordIndx=-1
             while pos!=-1:
                 textWithTarget=Target.split()[0]
                 for word in sentence[wordIndx+1:len(sentence)]:
-                    if textWithTarget in word:
+                    if textWithTarget in word.lower():
                         textWithTarget= word
                         break
-                wordIndx+=sentence[wordIndx+1:len(sentence)].index(textWithTarget)+1
+                wordIndx+=sentence[wordIndx+1:len(sentence)].find(textWithTarget)+1
+                print(wordIndx)
                 SearchResult.append(((sentence_num,wordIndx),s,pos,pos+len(Target)))
                 pos=s.find(Target,pos+1)
         
@@ -117,7 +147,7 @@ class SmartTextAnalyzer():
             return
         print("\x1b[1mResults Count: ",len(searchResult),"\n","="*50,"\x1b[0m",sep="",end="\n")
         for X in searchResult:
-            print(f'{ITALIC("Sentence Index:")} {BOLD(X[0][0])}\t| {ITALIC("Word Index:")} {BOLD(X[0][1])}\n"{X[1][0:X[2]]}{GREEN(ITALIC(UNDERLINED(X[1][X[2]:X[3]])))}{X[1][X[3]:len(X[1])]}"')
+            print(f'{ITALIC("Sentence Index:")} {BOLD(X[0][0])}\t, {ITALIC("Word Index:")} {BOLD(X[0][1])}\n"{X[1][0:X[2]]}{GREEN(ITALIC(UNDERLINED(X[1][X[2]:X[3]])))}{X[1][X[3]:len(X[1])]}"')
             print("-"*25)
 ########################################################################################
 #      Replacing Words/subStr
@@ -164,7 +194,7 @@ class SmartTextAnalyzer():
             for ind,(w,f) in enumerate(Words):
                 x="="
                 color=allColors.GetColor(reverse=True)
-                print(BlackBG(w.ljust(25)+color(x*(round(7*f)*10)))+BlackBG(color(" %.2f | %2.2f"%(W[ind][1],f*100)+"% ")))
+                print(BlackBG(w.ljust(25)+color(x*(round(7*f)*10)))+BlackBG(color(" %.2f , %2.2f"%(W[ind][1],f*100)+"% ")))
         if Vis:
             TakenPOS=self.generate_compact_positions(Words,max_positions=len(Words))
             for ind in range(len(TakenPOS)):
@@ -185,8 +215,7 @@ class SmartTextAnalyzer():
     def generate_compact_positions(self,data, max_positions=15):
         """
         Generate organized positions for compact word cloud design
-        Returns a list of (row, col) positions that work with your existing code
-        """
+        Returns a list of (row, col) positions that work with your existing code"""
         size = shutil.get_terminal_size()
         middle_line = size.lines // 2
         middle_col = size.columns // 2
@@ -227,9 +256,34 @@ class SmartTextAnalyzer():
             ring += 1
         return positions[:max_positions]
 
+########################################################################################
+#       predict next word method
+#---------------------------------------------------------------------------------------
+    def predict_next_topN(self,context_words, k=3):
+        context_words = tuple(context_words)
+        for i in range(len(context_words), 0, -1):
+            context = context_words[-i:]
+            if context in self.ngram:
+                next_words = self.ngram[context]
+                sorted_words = sorted(next_words.items(), key=lambda item: item[1], reverse=True)
+                return [word for word, freq in sorted_words[:k]]
+        return []
+    
+    def add_ngram(self,wordsInSentence):
+        for n in range(len(wordsInSentence),1,-1):#this would get me all possile gframs from 2 to the len of sentence
+            for i in range(len(wordsInSentence) - n + 1):
+                context = tuple(wordsInSentence[i:i + n - 1])
+                next_word = wordsInSentence[i + n - 1]
+                if context not in self.ngram:
+                    self.ngram[context] = {}
+                if next_word not in self.ngram[context]:
+                    self.ngram[context][next_word] = 0
+                self.ngram[context][next_word] += 1
+
 
 if __name__=="__main__":
-    file=open("SpaceIpsum.txt").read()
-    Report=SmartTextAnalyzer(file)
-    # Report.WordCloud(30,True,Vis=True) #Making a cloud that has 30 Words
-    # Report.WordCloud(30,True,Bar=True) #displaying freq in bar manner
+    with open("SpaceIpsum.txt", "r", encoding="utf-8") as f:
+        text = f.read()
+    # text="she hadn't"
+    Report=SmartTextAnalyzer(text)
+    print(Report.predict_next_topN(["no"]))
